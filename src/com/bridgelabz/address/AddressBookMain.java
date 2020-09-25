cc9a7a68c39556a05582d8b499722931915dbed8
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-09-25 22:50:27 +0530

    [Prerna]ADDED: Welcome Message

---
+++ src/com/bridgelabz/address/AddressBookMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.address;
+import java.util.*;
+
+public class AddressBookMain {
+	public static void main(String args[]) 
+	{
+		System.out.println("Welcome to Address Book Program!");
+	}
+
+}
