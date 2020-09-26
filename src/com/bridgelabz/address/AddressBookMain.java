4cc8e00de0add7a053edcfdf1c35958558aa835e
prernaraghuwanshi <raghuwanshi.prerna@gmail.com>
2020-09-26 11:54:03 +0530

    [Prerna]ADDED: Function to edit contact details

--- src/com/bridgelabz/address/AddressBookMain.java
+++ src/com/bridgelabz/address/AddressBookMain.java
@@ -5,10 +5,13 @@ public class AddressBookMain {
 	
 	Scanner sc = new Scanner(System.in);
 	ArrayList<Contacts> contactList;
+	HashMap<String,Contacts> contactMap;
+	private static Scanner i;
 	
 	public AddressBookMain()
 	{
 		contactList = new ArrayList<Contacts>();
+		contactMap = new HashMap<String,Contacts>();
 	}
 	public void addContact()
 	{
@@ -32,15 +35,76 @@ public class AddressBookMain {
 		
 		Contacts c = new Contacts(firstName, lastName, address, city, state, zip, phone, email);
 		contactList.add(c);
+		contactMap.put(firstName, c);
 		System.out.println("-----Contact Added Successfully-----");
 		System.out.println("Number of contacts in Address Book: " + contactList.size());
+	}
+	
+	public void editContact(String firstNameEdit)
+	{
+		System.out.println("-----Edit Contact-----");
+		
+		if(contactMap.containsKey(firstNameEdit))
+		{
+			Contacts cedit = contactMap.get(firstNameEdit);
+			System.out.println("Enter First Name: ");
+			cedit.firstName = sc.nextLine();
+			System.out.println("Enter Last Name: ");
+			cedit.lastName = sc.nextLine();
+			System.out.println("Enter Address: ");
+			cedit.address = sc.nextLine();
+			System.out.println("Enter City: ");
+			cedit.city = sc.nextLine();
+			System.out.println("Enter State:");
+			cedit.state = sc.nextLine();
+			System.out.println("Enter ZIP Code: ");
+			cedit.zip = sc.nextLine();
+			System.out.println("Enter Phone Number: ");
+			cedit.phoneNo =sc.nextLine();
+			System.out.println("Enter Email Address: ");
+			cedit.email = sc.nextLine();
+			System.out.println("-----Contact Details Updated-----");
+			System.out.println("Number of contacts in Address Book: " + contactList.size());
+			
+		}
+		else
+			System.out.println("-----Name not found in Address Book-----");
+		
+		
 	}
 	
 	public static void main(String args[]) 
 	{
 		System.out.println("Welcome to Address Book Program!");
+		
 		AddressBookMain a = new AddressBookMain();
-		a.addContact();
+		boolean flag =true;
+		while(flag)
+		{
+			System.out.println("Choose action on Address Book \n\n 1. Add Contact \n 2. Edit Contact \n 3. Exit");
+			i = new Scanner(System.in);
+			int choice = i.nextInt();
+			switch(choice)
+			{
+				case 1:
+					//Add contact
+					a.addContact();
+					break;
+				case 2:
+					//Edit contact
+					System.out.println("Enter first name of contact to be edited: ");
+					Scanner s= new Scanner(System.in);
+					String firstNameEdit = s.nextLine();
+					a.editContact(firstNameEdit);
+					break;
+				case 3:
+					System.out.println("-----Exiting from Address Book System-----");
+					flag = false;
+					break;
+					
+					
+			}
+		}
 	}
 
 }
